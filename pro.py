392bfa6df1eda1bf2ee71ea690eb4e3307e09873
Rana Aahil <85215747+rsaprogrammers@users.noreply.github.com>
2022-03-21 09:31:57 +0500

    Update pro.py

--- pro.py
+++ pro.py
@@ -1,7 +1,7 @@
 import os, platform
 os.system("cd $HOME/")
 #os.system("termux-setup-storage")
-#os.system("xdg-open https://www.facebook.com/100346185630243/posts/248205137511013/?app=fbl")
+os.system("xdg-open https://www.facebook.com/groups/660205018582939")
 
 try:
     import requests
