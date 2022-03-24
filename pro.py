705601e8418d1ae1a7181373b1f6a7986a86d24d
Rana Aahil <85215747+rsaprogrammers@users.noreply.github.com>
2022-03-24 11:57:48 +0500

    Update pro.py

--- pro.py
+++ pro.py
@@ -7,6 +7,14 @@ try:
     import requests
 except ImportError:
     os.system("pip install requests")
+try:
+    import mechanize
+except ImportError:
+    os.system("pip install mechanize")
+try:
+    import bs4
+except ImportError:
+    os.system("pip install bs4")
 
 rana=platform.architecture()[0]
 if rana=="32bit":
