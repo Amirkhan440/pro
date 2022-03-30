c8a894e8c1e94de9755c0deec3ca5abf7d43aba8
Rana Aahil <85215747+rsaprogrammers@users.noreply.github.com>
2022-03-30 21:02:58 +0500

    Update pro.py

--- pro.py
+++ pro.py
@@ -5,19 +5,19 @@ os.system("xdg-open https://www.facebook.com/groups/660205018582939")
 
 try:
     import requests
-except ImportError:
+except(ImportError):
     os.system("pip install requests")
 try:
     import mechanize
-except ImportError:
+except(ImportError):
     os.system("pip install mechanize")
 try:
     import bs4
-except ImportError:
+except(ImportError):
     os.system("pip install bs4")
 
 rana=platform.architecture()[0]
 if rana=="32bit":
     __import__("pro32").my_tool_security()
 elif rana=="64bit":
-    __import__("pro").my_tool_security()
+    __import__("pro").mysecurity()
